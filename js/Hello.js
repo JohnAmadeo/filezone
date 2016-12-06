c681de50a1fb5490f4da047c43b5895e2ebb5838
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2016-12-06 15:54:34 -0500

    Prototype page scrolling in PDF viewer

--- js/Hello.js
+++ js/Hello.js
@@ -5,6 +5,9 @@ import Request from 'superagent';
 import PDF from 'react-pdf-js';
 
 var Hello = React.createClass({
+  getInitialState: function() {
+    return {page: 1}
+  },
   onDrop: function(acceptedFiles, rejectedFiles) {
     var req = Request.post('/upload');
     console.log(req);
@@ -23,6 +26,30 @@ var Hello = React.createClass({
   onPageComplete(page) {
     this.setState({page})
   },
+  handlePrevious() {
+    this.setState({page: this.state.page - 1});
+  },
+  handleNext() {
+    this.setState({page: this.state.page + 1});
+  },
+  renderPagination(page, pages) {
+    let previousButton = <li className="previous" onClick={this.handlePrevious}><a href="#"><i className="fa fa-arrow-left"></i> Previous</a></li>;
+    if (page === 1) {
+      previousButton = <li className="previous disabled"><a href="#"><i className="fa fa-arrow-left"></i> Previous</a></li>;
+    }
+    let nextButton = <li className="next" onClick={this.handleNext}><a href="#">Next <i className="fa fa-arrow-right"></i></a></li>;
+    if (page === pages) {
+      nextButton = <li className="next disabled"><a href="#">Next <i className="fa fa-arrow-right"></i></a></li>;
+    }
+    return (
+      <nav>
+        <ul className="pager">
+          {previousButton}
+          {nextButton}
+        </ul>
+      </nav>
+      );
+  },
   render() {
   {/*
     return (
@@ -35,12 +62,22 @@ var Hello = React.createClass({
       </div>
     )
   }*/}
+    var pagination = null;
+    if(this.state.pages) {
+      pagination = this.renderPagination(this.state.page, this.state.pages);
+    }
+
     return (
       <div>
         <PDF  file="/hw1.pdf"               
               onDocumentComplete={this.onDocumentComplete} 
               onPageComplete={this.onPageComplete}
+              page={1} />
+        <PDF  file="/hw1.pdf"               
+              onDocumentComplete={this.onDocumentComplete} 
+              onPageComplete={this.onPageComplete}
               page={2} />
+        {pagination}
       </div>
     )
   }
